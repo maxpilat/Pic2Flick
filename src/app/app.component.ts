00614800c8ef716f34481afc5d98d0ece0fa9bbd
pilat_m <pilat_m@lwo.by>
2024-08-26 12:01:12 +0300

    modified:   src/app/app.component.html 	modified:   src/app/app.component.scss 	modified:   src/app/app.component.ts 	new file:   src/app/components/counter/counter.component.html 	new file:   src/app/components/counter/counter.component.scss 	new file:   src/app/components/counter/counter.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { select, Store } from '@ngrx/store';
-import { Observable, map } from 'rxjs';
-import { selectCount, selectUpdatedAt } from './store/selectors/counter.selectors';
-import { CommonModule } from '@angular/common';
-import { increment, decrement, clear } from './store/actions/counter.actions';
-import { CounterState } from './store/reducers/counter.reducer';
+import { CounterComponent } from './components/counter/counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  public count$: Observable<number>;
-  public updatedAt$: Observable<number>;
-  public disableDecrease$: Observable<boolean>;
-
-  constructor(private store: Store<CounterState>) {
-    this.count$ = this.store.pipe(select(selectCount));
-    this.updatedAt$ = this.store.pipe(select(selectUpdatedAt));
-    this.disableDecrease$ = this.count$.pipe(map((count) => count <= 0));
-  }
-
-  increase() {
-    this.store.dispatch(increment());
-  }
-
-  decrease() {
-    this.store.dispatch(decrement());
-  }
-
-  clear() {
-    this.store.dispatch(clear());
-  }
+  constructor() {}
 }
---
+++ src/app/components/counter/counter.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { map, Observable } from 'rxjs';
+import { selectCount, selectUpdatedAt } from '../../store/selectors/counter.selectors';
+import { CounterState } from '../../store/reducers/counter.reducer';
+import { increment, decrement, clear } from '../../store/actions/counter.actions';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.scss',
+})
+export class CounterComponent {
+  public count$: Observable<number>;
+  public updatedAt$: Observable<number>;
+  public disableDecrease$: Observable<boolean>;
+
+  constructor(private store: Store<CounterState>) {
+    this.count$ = this.store.pipe(select(selectCount));
+    this.updatedAt$ = this.store.pipe(select(selectUpdatedAt));
+    this.disableDecrease$ = this.count$.pipe(map((count: number) => count <= 0));
+  }
+
+  increase() {
+    this.store.dispatch(increment());
+  }
+
+  decrease() {
+    this.store.dispatch(decrement());
+  }
+
+  clear() {
+    this.store.dispatch(clear());
+  }
+}
