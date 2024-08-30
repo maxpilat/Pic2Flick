132088802e07a876e62857f8d6bf3c44e85a4336
MaksimPilat <pilatmd@outlook.com>
2024-08-30 17:26:01 +0300

    modified:   src/app/app.component.scss 	modified:   src/app/components/counter/counter.component.scss 	modified:   src/app/components/pin-gallery/pin-gallery.component.html 	modified:   src/app/components/pin/pin.component.html 	modified:   src/app/components/pin/pin.component.scss 	modified:   src/app/components/pin/pin.component.ts 	modified:   src/styles.scss

--- src/app/components/pin/pin.component.ts
+++ src/app/components/pin/pin.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { Pin } from '../../store/models/pin.model';
 
 @Component({
@@ -10,7 +10,26 @@ import { Pin } from '../../store/models/pin.model';
 })
 export class PinComponent {
   @Input() pin!: Pin;
-  @Output() imageLoaded = new EventEmitter<void>();
+  @Output() private imageLoaded = new EventEmitter<void>();
+  @ViewChild('pinElem') private pinRef: ElementRef;
+
+  ngAfterViewInit() {
+    this.calcPinWidth();
+  }
+
+  private calcPinWidth() {
+    const minWidth = 200;
+    const maxWidth = 300;
+    const margin = 22;
+
+    let numPins = Math.floor(window.innerWidth / (minWidth + margin));
+    let pinWidth = window.innerWidth / numPins - margin;
+    if (pinWidth > maxWidth) {
+      pinWidth = window.innerWidth / ++numPins - margin;
+    }
+
+    this.pinRef.nativeElement.style.width = `${pinWidth}px`;
+  }
 
   onImageLoad() {
     this.imageLoaded.emit();
